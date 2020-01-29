a4845cffe9f986848e54dc5e0cd6bbcacbf25ec0
Sven Lüpke <luepke@in.tum.de>
2020-01-29 14:05:13 +0100

    Added quality setting for each scene

---
+++ Assets/Scripts/ShadowConfigWoods.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowConfigWoods : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        QualitySettings.SetQualityLevel(6);
+    }
+}
